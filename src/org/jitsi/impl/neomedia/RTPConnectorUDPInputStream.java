8eece6ac719e040512d4b77725a9a7b1f2f6f612
Peter O'Neill <peter.oneill@metaswitch.com>
2013-04-29 17:18:02 +0100

    Remove media dump at the end of every call

--- src/org/jitsi/impl/neomedia/RTPConnectorUDPInputStream.java
+++ src/org/jitsi/impl/neomedia/RTPConnectorUDPInputStream.java
@@ -59,7 +59,6 @@ public class RTPConnectorUDPInputStream
         if(socket != null)
         {
             socket.close();
-            LibJitsi.getPacketLoggingService().dumpMediaBuffer();
         }
     }
 
