Huangzefeng/libjitsi-1

commit 46da52f3b9332a9676b8800aacf897f1647a2c2d

SFR 460602 Config lost.  Stop using FailSafeTransaction for config file. Instead use new TransactionBasedFile.  Rather than relying on copying multiple backups of the original and new files, instead just make new writes to a temporary file; and then save the result by (atomically) moving it over the top of the original when the changes are complete.

src/org/jitsi/service/fileaccess/TransactionBasedFile.java
@@ -1,0 +1,147 @@
+package org.jitsi.service.fileaccess;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+
+import org.jitsi.util.Logger;
+
+/**
+ * TransactionBasedFile.
+ * <p>
+ * Provides safe write access to an underlying file.  Safety is ensured by all
+ * writes going to a temp file, which is then moved over the original when <tt>
+ * commitTransaction</tt> is called.
+ * <p>
+ * Usage:<br>
+ * <ul>
+ * <li>Create a TransactionBasedFile for the file</li>
+ * <li>Call <tt>beginTransaction</tt></li>
+ * <li>Call <tt>getOutputStream</tt> and write to the returned stream</li>
+ * <li>If happy with the changes, call <tt>commitTransaction</tt> to save the
+ * result</li>
+ * <li>If unhappy, call <tt>abortTransaction</tt> to discard the changes</li>
+ * </ul>
+ */
+public class TransactionBasedFile
+{
+    private static final String TEMP_SUFFIX = ".tmp";
+
+    private static final Logger logger = Logger.getLogger(TransactionBasedFile.class);
+
+    private File mTempFile;
+    private File mFile;
+    private Boolean transactionInProgress;
+
+    /**
+     * Create a new TransactionBasedFile
+     * @param file The file to wrap
+     */
+    public TransactionBasedFile(File file)
+    {
+        mFile = file;
+        logger.debug("Create transaction-based file: " + file.getAbsolutePath());
+        transactionInProgress = false;
+    }
+
+    /**
+     * Start a new transaction.  Must be closed by <tt>commitTransaction</tt> or
+     * <tt>abortTransaction</tt> before the next transaction can begin.  User
+     * can now call <tt>getOutputStream</tt> and write data.
+     */
+    public void beginTransaction()
+    {
+        synchronized(transactionInProgress)
+        {
+            if (transactionInProgress)
+            {
+                logger.error("Transaction already in progress");
+                throw new IllegalStateException("Transaction already in progress");
+            }
+
+            transactionInProgress = true;
+            mTempFile = new File(mFile.getAbsolutePath() + TEMP_SUFFIX);
+        }
+    }
+
+    /**
+     * Save all writes to the underlying file and end the transaction.
+     */
+    public void commitTransaction()
+    {
+        synchronized(transactionInProgress)
+        {
+            if (!transactionInProgress)
+            {
+                logger.error("No transaction to commit");
+                throw new IllegalStateException("No transaction to commit");
+            }
+
+            try
+            {
+                Files.move(mTempFile.toPath(),
+                           mFile.toPath(),
+                           StandardCopyOption.ATOMIC_MOVE);
+            }
+            catch (IOException ioex)
+            {
+                logger.error("Couldn't commit transaction", ioex);
+            }
+
+            transactionInProgress = false;
+        }
+
+        mTempFile = null;
+    }
+
+    /**
+     * Abandon all writes in this transaction, and end the transaction.
+     */
+    public void abortTransaction()
+    {
+        synchronized(transactionInProgress)
+        {
+            if (!transactionInProgress)
+            {
+                logger.error("No transaction to abort");
+                throw new IllegalStateException("No transaction to abort");
+            }
+
+            mTempFile.delete();
+            mTempFile = null;
+
+            transactionInProgress = false;
+        }
+    }
+
+    /**
+     * Obtain an OutputStream to which changes can be written.  Can only be
+     * called while a transaction is active.
+     * @return The OutputStream.  <b>Caller is responsible for closing this.</b>
+     */
+    public OutputStream getOutputStream()
+    {
+        synchronized(transactionInProgress)
+        {
+            if (!transactionInProgress)
+            {
+                logger.error("No transaction - not possible to get output stream");
+                throw new IllegalStateException("No transaction - not possible to get output stream");
+            }
+
+            try
+            {
+                return new FileOutputStream(mTempFile);
+            }
+            catch (FileNotFoundException e)
+            {
+                logger.error("Couldn't get file output stream for temp file");
+                throw new IllegalStateException(e);
+            }
+        }
+    }
+}

src/org/jitsi/impl/configuration/ConfigurationServiceImpl.java
@@ -112,7 +112,7 @@ public class ConfigurationServiceImpl
      * The transaction object used to ensure that updates to configuration are
      * made atomically, and can be rolled back if errors occur.
      */
-    private FailSafeTransaction failSafeTransaction;
+    private TransactionBasedFile transactionBasedFile;
 
     /**
      * The <code>ConfigurationStore</code> implementation which contains the
@@ -126,6 +126,10 @@ public class ConfigurationServiceImpl
     {
         // retrieve a reference to the FileAccessService
         this.faService = LibJitsi.getFileAccessService();
+        if (faService == null)
+        {
+            logger.error("Couldn't get access to FileAccessService");
+        }
 
         try
         {
@@ -156,6 +160,7 @@ public class ConfigurationServiceImpl
      * @throws ConfigPropertyVetoException in case someone is not happy with the
      * change.
      */
+    @Override
     public void setProperty(String propertyName, Object property)
         throws ConfigPropertyVetoException
     {
@@ -181,6 +186,7 @@ public class ConfigurationServiceImpl
      * @throws ConfigPropertyVetoException in case someone is not happy with the
      * change.
      */
+    @Override
     public void setProperty(String propertyName,
                             Object property,
                             boolean isSystem)
@@ -229,6 +235,7 @@ public class ConfigurationServiceImpl
      * @throws ConfigPropertyVetoException in case someone is not happy with the
      * change.
      */
+    @Override
     public void setProperties(Map<String, Object> properties)
         throws ConfigPropertyVetoException
     {
@@ -341,6 +348,7 @@ public class ConfigurationServiceImpl
      * <p>
      * @param propertyName the name of the property to change.
      */
+    @Override
     public void removeProperty(String propertyName)
     {
         List<String> childPropertyNames =
@@ -461,6 +469,7 @@ public class ConfigurationServiceImpl
      * @param propertyName the name of the property that is being queried.
      * @return the value of the property with the specified name.
      */
+    @Override
     public Object getProperty(String propertyName)
     {
         Object result = immutableDefaultProperties.get(propertyName);
@@ -482,6 +491,7 @@ public class ConfigurationServiceImpl
      *
      * @return a <tt>java.util.List</tt>containing all property names
      */
+    @Override
     public List<String> getAllPropertyNames()
     {
         List<String> resultKeySet = new LinkedList<String>();
@@ -527,6 +537,7 @@ public class ConfigurationServiceImpl
      * @return a <tt>java.util.List</tt>containing all property name String-s
      * matching the specified conditions.
      */
+    @Override
     public List<String> getPropertyNamesByPrefix(String prefix,
             boolean exactPrefixMatch)
     {
@@ -643,6 +654,7 @@ public class ConfigurationServiceImpl
      * @return a <tt>List</tt> of <tt>String</tt>s containing the property names
      * which contain the specified <tt>suffix</tt>
      */
+    @Override
     public List<String> getPropertyNamesBySuffix(String suffix)
     {
         List<String> resultKeySet = new LinkedList<String>();
@@ -662,6 +674,7 @@ public class ConfigurationServiceImpl
      *
      * @param listener the PropertyChangeListener to be added
      */
+    @Override
     public void addPropertyChangeListener(PropertyChangeListener listener)
     {
         changeEventDispatcher.addPropertyChangeListener(listener);
@@ -672,6 +685,7 @@ public class ConfigurationServiceImpl
      *
      * @param listener the PropertyChangeListener to be removed
      */
+    @Override
     public void removePropertyChangeListener(PropertyChangeListener listener)
     {
         changeEventDispatcher.removePropertyChangeListener(listener);
@@ -684,6 +698,7 @@ public class ConfigurationServiceImpl
      * @param propertyName one of the property names listed above
      * @param listener the PropertyChangeListener to be added
      */
+    @Override
     public void addPropertyChangeListener(String propertyName,
                                           PropertyChangeListener listener)
     {
@@ -698,6 +713,7 @@ public class ConfigurationServiceImpl
      * @param propertyName a valid property name
      * @param listener the PropertyChangeListener to be removed
      */
+    @Override
     public void removePropertyChangeListener(String propertyName,
                                              PropertyChangeListener listener)
     {
@@ -710,6 +726,7 @@ public class ConfigurationServiceImpl
      *
      * @param listener the VetoableChangeListener to be added
      */
+    @Override
     public void addVetoableChangeListener(ConfigVetoableChangeListener listener)
     {
         changeEventDispatcher.addVetoableChangeListener(listener);
@@ -720,6 +737,7 @@ public class ConfigurationServiceImpl
      *
      * @param listener the VetoableChangeListener to be removed
      */
+    @Override
     public void removeVetoableChangeListener(ConfigVetoableChangeListener listener)
     {
         changeEventDispatcher.removeVetoableChangeListener(listener);
@@ -732,6 +750,7 @@ public class ConfigurationServiceImpl
      * @param propertyName one of the property names listed above
      * @param listener the VetoableChangeListener to be added
      */
+    @Override
     public void addVetoableChangeListener(String propertyName,
             ConfigVetoableChangeListener listener)
     {
@@ -745,6 +764,7 @@ public class ConfigurationServiceImpl
      * @param propertyName a valid property name
      * @param listener the VetoableChangeListener to be removed
      */
+    @Override
     public void removeVetoableChangeListener(String propertyName,
             ConfigVetoableChangeListener listener)
     {
@@ -755,27 +775,13 @@ public class ConfigurationServiceImpl
     /*
      * Implements ConfigurationService#reloadConfiguration().
      */
+    @Override
     public void reloadConfiguration()
         throws IOException
     {
         this.configurationFile = null;
 
         File file = getConfigurationFile();
-
-        if ((file != null) && (faService != null))
-        {
-            // Restore the file if necessary.
-            try
-            {
-                failSafeTransaction.restoreFile();
-            }
-            catch (Exception e)
-            {
-                logger.error(
-                        "Failed to restore configuration file " + file,
-                        e);
-            }
-        }
 
         try
         {
@@ -793,6 +799,7 @@ public class ConfigurationServiceImpl
     /*
      * Implements ConfigurationService#storeConfiguration().
      */
+    @Override
     public synchronized void storeConfiguration()
         throws IOException
     {
@@ -816,12 +823,10 @@ public class ConfigurationServiceImpl
 
         try
         {
-            if (failSafeTransaction != null)
-                failSafeTransaction.beginTransaction();
+            if (transactionBasedFile != null)
+                transactionBasedFile.beginTransaction();
 
-            OutputStream stream = (configurationFile == null) ?
-                                        null :
-                                        new FileOutputStream(configurationFile);
+            OutputStream stream = transactionBasedFile.getOutputStream();
 
             try
             {
@@ -833,8 +838,8 @@ public class ConfigurationServiceImpl
                     stream.close();
             }
 
-            if (failSafeTransaction != null)
-                failSafeTransaction.commit();
+            if (transactionBasedFile != null)
+                transactionBasedFile.commitTransaction();
         }
         catch (IllegalStateException isex)
         {
@@ -850,11 +855,11 @@ public class ConfigurationServiceImpl
             logger.error(
                     "can't write data in the configuration file",
                     exception);
-            if (failSafeTransaction != null)
+            if (transactionBasedFile != null)
             {
                 try
                 {
-                    failSafeTransaction.rollback();
+                    transactionBasedFile.abortTransaction();
                 }
                 catch (IllegalStateException isex)
                 {
@@ -871,6 +876,7 @@ public class ConfigurationServiceImpl
      * {@link #getScHomeDirName()}
      * @return  the name of the configuration file currently used.
      */
+    @Override
     public String getConfigurationFilename()
     {
         try
@@ -912,11 +918,10 @@ public class ConfigurationServiceImpl
             getScHomeDirName();
         }
 
-        if ((failSafeTransaction == null) &&
+        if ((transactionBasedFile == null) &&
             (faService != null))
         {
-            failSafeTransaction =
-                    faService.createFailSafeTransaction(this.configurationFile);
+            transactionBasedFile = new TransactionBasedFile(configurationFile);
         }
 
         return this.configurationFile;
@@ -1128,6 +1133,7 @@ public class ConfigurationServiceImpl
      * user specific data such as configuration files, message and call history
      * as well as is bundle repository.
      */
+    @Override
     public String getScHomeDirLocation()
     {
         //first let's check whether we already have the name of the directory
@@ -1169,6 +1175,7 @@ public class ConfigurationServiceImpl
      * user specific data such as configuration files, message and call history
      * as well as is bundle repository.
      */
+    @Override
     public String getScHomeDirName()
     {
         //first let's check whether we already have the name of the directory
@@ -1430,6 +1437,7 @@ public class ConfigurationServiceImpl
      * <tt>propertyName</tt>, or the returned string had zero length or
      * contained whitespaces only.
      */
+    @Override
     public String getString(String propertyName)
     {
         Object propValue = getProperty(propertyName);
@@ -1458,6 +1466,7 @@ public class ConfigurationServiceImpl
      * the specified <tt>propertyName</tt>, or the returned string had zero
      * length or contained whitespaces only.
      */
+    @Override
     public String getString(String propertyName, String defaultValue)
     {
         String value = getString(propertyName);
@@ -1467,6 +1476,7 @@ public class ConfigurationServiceImpl
     /*
      * Implements ConfigurationService#getBoolean(String, boolean).
      */
+    @Override
     public boolean getBoolean(String propertyName, boolean defaultValue)
     {
         String stringValue = getString(propertyName);
@@ -1496,6 +1506,7 @@ public class ConfigurationServiceImpl
      * fails or no value is associated in this <tt>ConfigurationService</tt>
      * with the specified property name
      */
+    @Override
     public int getInt(String propertyName, int defaultValue)
     {
         String stringValue = getString(propertyName);
@@ -1538,6 +1549,7 @@ public class ConfigurationServiceImpl
      * fails or no value is associated in this <tt>ConfigurationService</tt>
      * with the specified property name
      */
+    @Override
     public long getLong(String propertyName, long defaultValue)
     {
         String stringValue = getString(propertyName);
@@ -1579,8 +1591,10 @@ public class ConfigurationServiceImpl
     /**
      * Deletes the configuration file currently used by this implementation.
      */
+    @Override
     public void purgeStoredConfiguration()
     {
+        logger.warn("Purging stored configuration! Stack: ", new Throwable("Call stack:"));
         if (configurationFile != null)
         {
             configurationFile.delete();

src/org/jitsi/service/fileaccess/FailSafeTransaction.java
@@ -50,7 +50,7 @@ public interface FailSafeTransaction {
         throws IllegalStateException, IOException;
 
     /**
-     * Closes the transation and cancel the changes. Everything written in the
+     * Closes the transaction and cancel the changes. Everything written in the
      * file during the transaction is NOT saved.
      * @throws IllegalStateException if the file doesn't exists anymore
      * @throws IOException if an IOException occurs during the operation

commit 9fefff32f3eccf0b7a79a673ad5fdf1939d83cec

Fix hang on startup by not syncing on Boolean. Caused by 46da52f3b9332a9676b8800aacf897f1647a2c2d Fixed by a7325792ce4bf0ac8300a010ec79bddc04475100 Reinstated by aadbe648c8048f5e4717f3ceafab910fee313ce5

src/org/jitsi/service/fileaccess/TransactionBasedFile.java
@@ -35,7 +35,7 @@ public class TransactionBasedFile
 
     private File mTempFile;
     private File mFile;
-    private Boolean transactionInProgress;
+    private boolean transactionInProgress;
 
     /**
      * Create a new TransactionBasedFile
@@ -53,69 +53,59 @@ public class TransactionBasedFile
      * <tt>abortTransaction</tt> before the next transaction can begin.  User
      * can now call <tt>getOutputStream</tt> and write data.
      */
-    public void beginTransaction()
+    public synchronized void beginTransaction()
     {
-        synchronized(transactionInProgress)
+        if (transactionInProgress)
         {
-            if (transactionInProgress)
-            {
-                logger.error("Transaction already in progress");
-                throw new IllegalStateException("Transaction already in progress");
-            }
-
-            transactionInProgress = true;
-            mTempFile = new File(mFile.getAbsolutePath() + TEMP_SUFFIX);
+            logger.error("Transaction already in progress");
+            throw new IllegalStateException("Transaction already in progress");
         }
+
+        transactionInProgress = true;
+        mTempFile = new File(mFile.getAbsolutePath() + TEMP_SUFFIX);
     }
 
     /**
      * Save all writes to the underlying file and end the transaction.
      */
-    public void commitTransaction()
+    public synchronized void commitTransaction()
     {
-        synchronized(transactionInProgress)
+        if (!transactionInProgress)
         {
-            if (!transactionInProgress)
-            {
-                logger.error("No transaction to commit");
-                throw new IllegalStateException("No transaction to commit");
-            }
+            logger.error("No transaction to commit");
+            throw new IllegalStateException("No transaction to commit");
+        }
 
-            try
-            {
-                Files.move(mTempFile.toPath(),
-                           mFile.toPath(),
-                           StandardCopyOption.ATOMIC_MOVE);
-            }
-            catch (IOException ioex)
-            {
-                logger.error("Couldn't commit transaction", ioex);
-            }
-
-            transactionInProgress = false;
+        try
+        {
+            Files.move(mTempFile.toPath(),
+                       mFile.toPath(),
+                       StandardCopyOption.ATOMIC_MOVE);
+        }
+        catch (IOException ioex)
+        {
+            logger.error("Couldn't commit transaction", ioex);
         }
 
         mTempFile = null;
+        transactionInProgress = false;
     }
 
     /**
      * Abandon all writes in this transaction, and end the transaction.
      */
-    public void abortTransaction()
+    public synchronized void abortTransaction()
     {
-        synchronized(transactionInProgress)
+        if (!transactionInProgress)
         {
-            if (!transactionInProgress)
-            {
-                logger.error("No transaction to abort");
-                throw new IllegalStateException("No transaction to abort");
-            }
-
-            mTempFile.delete();
-            mTempFile = null;
-
-            transactionInProgress = false;
+            logger.error("No transaction to abort");
+            throw new IllegalStateException("No transaction to abort");
         }
+
+        mTempFile.delete();
+        mTempFile = null;
+
+        transactionInProgress = false;
     }
 
     /**
@@ -123,25 +113,23 @@ public class TransactionBasedFile
      * called while a transaction is active.
      * @return The OutputStream.  <b>Caller is responsible for closing this.</b>
      */
-    public OutputStream getOutputStream()
+    public synchronized OutputStream getOutputStream()
     {
-        synchronized(transactionInProgress)
+        if (!transactionInProgress)
         {
-            if (!transactionInProgress)
-            {
-                logger.error("No transaction - not possible to get output stream");
-                throw new IllegalStateException("No transaction - not possible to get output stream");
-            }
+            logger.error("No transaction - not possible to get output stream");
+            throw new IllegalStateException("No transaction - not possible to get output stream");
+        }
 
-            try
-            {
-                return new FileOutputStream(mTempFile);
-            }
-            catch (FileNotFoundException e)
-            {
-                logger.error("Couldn't get file output stream for temp file");
-                throw new IllegalStateException(e);
-            }
+        try
+        {
+            return new FileOutputStream(mTempFile);
+        }
+        catch (FileNotFoundException e)
+        {
+            logger.error("Couldn't get file output stream for temp file: " +  mTempFile);
+            throw new IllegalStateException(
+                    "Couldn't get file output stream for temp file", e);
         }
     }
 }